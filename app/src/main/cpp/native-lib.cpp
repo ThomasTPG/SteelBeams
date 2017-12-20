94adfd3d25701eada72d0f7b8a02f41b45928b93
unknown <thomas.p.g@hotmail.co.uk>
2017-12-20 19:55:57 +0000

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,28 @@
+#include <jni.h>
+#include <string>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+extern "C" {
+
+
+JNIEXPORT jstring
+
+JNICALL
+Java_com_example_user_cmake_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+jstring
+Java_com_example_user_cmake_MainActivity_imageTest(JNIEnv *env, jobject, jlong addrImage)
+{
+    cv::Mat* inputImage_p = (cv::Mat*)addrImage;
+    cv::cvtColor(*inputImage_p, *inputImage_p, CV_BGR2GRAY);
+    std::string hello2 = "Hello from validate";
+    return env->NewStringUTF(hello2.c_str());
+}
+
+}
---
+++ app/src/main/java/com/example/user/cmake/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.user.cmake;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvException;
+import org.opencv.core.Mat;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java3" );
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        ImageView iv = (ImageView) findViewById(R.id.image_view);
+        Mat m = new Mat();
+        Bitmap rgbImage = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        Utils.bitmapToMat(rgbImage, m);
+        if (!OpenCVLoader.initDebug())
+        {
+            s += "Failed";
+        }
+        else
+        {
+            s += "Success" + imageTest(m.getNativeObjAddr());
+        }
+        Bitmap bmp = null;
+        try {
+            bmp = Bitmap.createBitmap(m.cols(), m.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(m, bmp);
+        }
+        catch (CvException e){}
+        iv.setImageBitmap(bmp);
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    public native String imageTest(long imageAddr);
+}
