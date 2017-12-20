b0a85dbaf43533d0288c9d63cfeacc66fc9dabd6
unknown <thomas.p.g@hotmail.co.uk>
2017-12-20 22:59:43 +0000

    Add buttons to take picture

--- app/src/main/java/com/example/user/cmake/MainActivity.java
+++ app/src/main/java/com/example/user/cmake/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.user.cmake;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,7 +17,13 @@ import org.opencv.android.Utils;
 import org.opencv.core.CvException;
 import org.opencv.core.Mat;
 
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
+
+    final static int PICK_PHOTO_FROM_GALLERY = 1;
+    final static int TAKE_CAMERA_PHOTO = 2;
+    Bitmap rgbImage;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -25,10 +36,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
+        setupButtons();
+    }
+
+    private void setupButtons()
+    {
+        Button gallery = (Button) findViewById(R.id.gallery_button);
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onGalleryButtonPress();
+            }
+        });
+
+
+        Button camera = (Button) findViewById(R.id.camera_button);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCameraPress();
+            }
+        });
+
+        Button start = (Button) findViewById(R.id.start_button);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onStartPress();
+            }
+        });
+    }
+
+    private void onStartPress()
+    {
         ImageView iv = (ImageView) findViewById(R.id.image_view);
         Mat m = new Mat();
-        Bitmap rgbImage = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
         Utils.bitmapToMat(rgbImage, m);
         imageTest(m.getNativeObjAddr());
         Bitmap bmp = null;
@@ -38,7 +80,42 @@ public class MainActivity extends AppCompatActivity {
         }
         catch (CvException e){}
         iv.setImageBitmap(bmp);
+    }
 
+    private void onGalleryButtonPress() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_PHOTO_FROM_GALLERY);
+    }
+
+    private void onCameraPress() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, TAKE_CAMERA_PHOTO);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_PHOTO_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                return;
+            }
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                rgbImage = BitmapFactory.decodeStream(inputStream);
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        if (requestCode == TAKE_CAMERA_PHOTO && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            rgbImage = (Bitmap) extras.get("data");
+        }
     }
 
     /**
